66065ae389b4593c1f5a77951105d2a2ac81176c
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-07-24 16:55:55 -0400

    add hook useCounter

---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,28 @@
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
+import { gsap } from "gsap"
+
+export const useCounter = ({maxCount = 1}) => {
+    const [counter, setCounter] = useState(5)
+  const elementToAnimate = useRef<HTMLHeadingElement>(null)
+
+  const tl = useRef(gsap.timeline())
+
+  const handleClick = () => {
+    setCounter(prev => Math.min(prev +1, maxCount))
+  }
+
+  useLayoutEffect(() => {
+    tl.current.to(elementToAnimate.current,{y: -10, duration: 0.2, ease: 'ease.out' })
+    .to(elementToAnimate.current,{y: 10, duration: 1, ease: 'bounce.out' })
+  }, [])
+
+  useEffect(() => {
+    tl.current.play(0)
+  }, [counter])
+
+  return{
+    counter,
+    elementToAnimate,
+    handleClick
+  }
+}
